459a8bbdeaae2dab71ff5177e4fa761f1f719ecb
IrisCZ <iriscarballo@gmail.com>
2017-01-18 19:07:13 +0100

    Static html added

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module UnzuluTime
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    if Rails.configuration.respond_to?(:sass)
+      Rails.configuration.sass.tap do |config|
+        config.preferred_syntax = :sass
+      end
+    end
   end
 end
