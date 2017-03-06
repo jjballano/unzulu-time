7a7233807b371160e6b058c4ca423af78e09b782
jjballano <jjballano@gmail.com>
2017-03-06 20:15:01 +0100

    Shows the task list done

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,14 @@ class Task < ApplicationRecord
     task_periods.any? { |p| p.started? }
   end
 
+  def start_date
+    task_periods.order(:started_at).first.started_at unless task_periods.empty?
+  end
+
+  def finish_date  
+    task_periods.order(:finished_at).last.finished_at unless started? || task_periods.empty?
+  end
+
   private
 
   def create_period 
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   
-  let!(:task) { build(:task) }
+  let!(:task) { create(:task) }
 
   describe 'create' do
     it 'should creates a task period with current time as start' do      
-
-      task.save
 
       expect(task.task_periods.size).to eq(1)
       
@@ -15,13 +13,42 @@ RSpec.describe Task, type: :model do
     end
 
     it 'should return started? value depends on if it has any task period started' do
-      expect(task.started?).to be_falsy
+      task.task_periods.first.started_at = Time.now
+      expect(task.started?).to be_truthy
+
+      task.task_periods.first.finished_at = Time.now
 
       task.task_periods.build(started_at: Time.now, finished_at: Time.now)
       expect(task.started?).to be_falsy
 
       task.task_periods.build(started_at: Time.now, finished_at: nil)
       expect(task.started?).to be_truthy
+    end
+
+    it 'should return the start date with the first of its task periods' do
+      now = Time.now
+      task.task_periods.first.started_at = now - 10
+      task.task_periods.build(started_at: now - 20)
+      task.task_periods.build(started_at: now - 5)
+
+      task.save
+
+      expect(task.start_date.to_i).to eq((now - 20).to_i)
+    end
+
+    it 'should return the end date with the latest of its task periods' do
+      now = Time.now
+      period = task.task_periods.first      
+      period.finished_at = now - 10
+      period.save      
+
+      task.task_periods.first.finished_at = now - 10
+      task.task_periods.build(started_at: now - 50, finished_at: now - 20)
+      task.task_periods.build(started_at: now - 10, finished_at: now - 5)
+
+      task.save
+
+      expect(task.finish_date.to_i).to eq((now - 5).to_i)
     end
   end
 end
