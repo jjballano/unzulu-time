d01538ec746482e5dbbe17725e220dafd724c939
jjballano <jjballano@gmail.com>
2017-04-13 18:16:38 +0200

    Fixed a problem with production environment as it havent loaded the lib folder automatically

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.17.57.44', user: 'deploy', roles: %w{web app db}
+server '34.249.215.193', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
